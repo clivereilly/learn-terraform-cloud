4698721238892a3c93a61058174c1c547848fcd4
root <clivereilly@hotmail.com>
2023-11-07 16:04:20 +0000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "clive-tf-cloud-org"
 
     workspaces {
       name = "learn-terraform-cloud"
@@ -14,6 +14,7 @@ terraform {
       version = "~> 3.28.0"
     }
   }
+*/
 
   required_version = ">= 0.14.0"
 }
